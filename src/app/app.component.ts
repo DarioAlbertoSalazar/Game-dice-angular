ec70755cd46abaafb5299f4b7938d3d0e150ab15
Darío Salazar Leal <dario12_58@hotmail.com>
2023-05-31 19:34:50 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Dados';
+  leftdice = '../assets/img/dice2.png';
+  rightdice = '../assets/img/dice1.png';
+  dice1 = 1;
+  dice2 = 2;
+
+  dropDice(): void {
+    this.dice1 = Math.round(Math.random() * 5) + 1;
+    this.dice2 = Math.round(Math.random() * 5) + 1;
+    this.leftdice = '../assets/img/dice' + this.dice1 + '.png';
+    this.rightdice = '../assets/img/dice' + this.dice2 + '.png';
+  }
 }
